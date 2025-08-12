3c159f1015a345c147a9d6a46d98f2ebdbd27671
Sviatko <dewifimyy@gmail.com>
2025-08-12 16:28:23 +0300

    add forms page new 1

--- pages/form_page.py
+++ pages/form_page.py
@@ -1,10 +1,15 @@
 import email
+import os
 import random
+from os import path
+from selenium.webdriver.remote.webdriver import WebDriver
 
 from pywin.framework.intpyapp import lastLocateFileName
-from selenium.webdriver import ActionChains
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common import keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -22,11 +27,11 @@ class FormPage():
     #GENDER = (By.CSS_SELECTOR, "label[for='gender-radio-2']")
     MOBILE = (By.ID, "userNumber")
     DATE_OF_BIRTH = (By.CSS_SELECTOR, "dateOfBirthInput")
-    SUBJECT = (By.CSS_SELECTOR, "subjectsInput")
-    HOBBIES = (By.CSS_SELECTOR, f"//input[@id='hobbies-checkbox-{random.randint(1, 3)}']")
-    FILE_INPUT = (By.CSS_SELECTOR, "uploadPicture")
-    CURRENT_ADDRESS = (By.CSS_SELECTOR, "currentAddress")
-    SELECT_STATE = (By.CSS_SELECTOR, "state")
+    SUBJECT = (By.ID, "subjectsInput")
+    HOBBIES = (By.CSS_SELECTOR, f"label[for='hobbies-checkbox-{random.randint(1, 3)}']")
+    FILE_INPUT = (By.ID, "uploadPicture")
+    CURRENT_ADDRESS = (By.ID, "currentAddress")
+    SELECT_STATE = (By.ID, "state")
     STATE_INPUT = (By.CSS_SELECTOR, "react-select-3-input")
     SELECT_CITY = (By.CSS_SELECTOR, "city")
     CITY_INPUT = (By.CSS_SELECTOR, "react-select-4-input")
@@ -41,5 +46,24 @@ class FormPage():
        self.wait.until(EC.visibility_of_element_located(self.EMAIL)).send_keys(email)
        self.wait.until(EC.visibility_of_element_located(self.MOBILE)).send_keys(mobile)
        self.wait.until(EC.visibility_of_element_located(self.GENDER)).click()
+       self.wait.until(EC.visibility_of_element_located(self.HOBBIES)).click()
+       self.wait.until(EC.visibility_of_element_located(self.SUBJECT)).send_keys('Maths')
+       self.wait.until(EC.visibility_of_element_located(self.SUBJECT)).send_keys(Keys.RETURN)
+       self.wait.until(EC.presence_of_element_located(self.FILE_INPUT)) \
+           .send_keys(r"C:\Users\Sviatko\Desktop\Англійська\зображення_viber_2025-07-24_13-35-51-948.jpg")
+       self.wait.until(EC.visibility_of_element_located(self.CURRENT_ADDRESS)).send_keys('Святіко')
+       self.wait.until(EC.presence_of_element_located(self.SELECT_STATE)).click()
 
+    # def select_state(self, state_name: str):
+    #     # Клік по контейнеру дропдауну
+    #     self.wait.until(EC.element_to_be_clickable(self.SELECT_STATE)).click()
+    #
+    #     # Ввести значення в інпут
+    #     self.wait.until(EC.visibility_of_element_located(self.STATE_INPUT)).send_keys("Haryana")
+    #
+    #     # Підтвердити вибір
+    #     self.wait.until(EC.visibility_of_element_located(self.STATE_INPUT)).send_keys(Keys.ENTER)
+    #
+    #
+    #
 
